35326436b67d4c4507a4a76fcabb27f20ccc14df
fonwash <fonwash@gmail.com>
2024-03-22 15:18:29 +0100

    update

--- includes/libtest.h
+++ includes/libtest.h
@@ -6,7 +6,7 @@
 /*   By: juperez <juperez@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/15 08:17:48 by juperez           #+#    #+#             */
-/*   Updated: 2024/03/22 15:07:47 by juperez          ###   ########.fr       */
+/*   Updated: 2024/03/22 15:18:24 by juperez          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,7 @@
 # include <stdbool.h>
 # include <stdio.h>
 # include <time.h>
+# include <string.h>
 
 // BUFFER
 # define BUFFER_SIZE 1024
