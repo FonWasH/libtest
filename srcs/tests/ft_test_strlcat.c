c4cf11ddbe4d3fa8a77aaf0215610df56bb8a567
fonwash <fonwash@gmail.com>
2024-03-28 16:52:41 +0100

    update strlcat

--- srcs/tests/ft_test_strlcat.c
+++ srcs/tests/ft_test_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: juperez <juperez@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/27 08:48:30 by juperez           #+#    #+#             */
-/*   Updated: 2024/03/28 15:46:36 by juperez          ###   ########.fr       */
+/*   Updated: 2024/03/28 16:51:44 by juperez          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,23 +14,25 @@
 
 bool	ft_test_strlcat(char *name, void **test)
 {
+	size_t	size[] = {0, 1, 2, 4, 8, 16, 32};
 	const char	*tests = test ? *(const char **)test : g_str_tests[1];
-	size_t		i = 0, grade = 0, user_r, libc_r;
-	char		user[] = "DEST\0................", libc[] = "DEST\0................";
+	size_t		i = 0, grade = 0, user_r, libc_r, count = sizeof(size) / sizeof(size[0]);
+	char		user[] = "DEST\0........................", libc[] = "DEST\0........................";
 	bool		success;
 
 	(void)name;
-	while (i < 20)
+	while (i < count)
 	{
 		ft_time_function(USER_START);
-		user_r = ft_strlcat(user, tests, i);
+		user_r = ft_strlcat(user, tests, size[i]);
 		ft_time_function(USER_END_LIBC_START);
-		libc_r = strlcat(libc, tests, i);
+		libc_r = strlcat(libc, tests, size[i]);
 		ft_time_function(LIBC_END);
+		printf("%s\n%s\n", user, libc);
 		success = (!strcmp(user, libc) && user_r == libc_r);
 		if (!success)
 		{
-			ft_result_input_sizet(i);
+			ft_result_input_sizet(size[i]);
 			ft_result_input_str((char *)tests, NULL);
 			ft_result_output_sizet(user_r, libc_r);
 			ft_result_output_str(user, libc);
